d7a81a5fd13d25b749a5d7b477a0ba6b21cb9533
savagerunderscorelex <eigbekoyi@gmail.com>
2025-09-14 16:55:48 -0400

    started working on the projects page, added the card for my python project, added the element <a> to the paragraph and list item rule

---
+++ scripts/projects.js
@@ -0,0 +1,31 @@
+var i = 0;
+var text = "Projects";
+var speed = 250;
+
+/* TypeWriter Animation */
+function typeWriter () {
+    if (i < text.length) {
+        document.getElementById("projectsheading").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+
+    }
+}
+
+/* Cursor Animation */
+var cursor = true;
+var speed = 350;
+
+setInterval(() => {
+    if (cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+    } else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+    }
+}, speed);
+
+/* Triggering Events */
+window.onload = setInterval; /* Cursor Animation */
+window.onload = typeWriter; /* Typewriter Animation */
